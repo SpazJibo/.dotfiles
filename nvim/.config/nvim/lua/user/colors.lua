2614b903c85031fc8c65654f4285fd717f186e6b
Curtis Allen Gibeaut Jr <curtjibo@gmail.com>
2023-08-29 17:25:46 -0500

    Setting Up proper settings for nvim

---
+++ nvim/.config/nvim/lua/user/colors.lua
@@ -0,0 +1,7 @@
+local colorscheme = "shades_of_purple"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
