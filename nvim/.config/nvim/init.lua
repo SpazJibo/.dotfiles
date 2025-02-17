92b4da47abc12818a566b93ceef31138bc20be03
Curtis Allen Gibeaut Jr <curtis@jibostudio.com>
2025-02-17 16:18:32 -0600

    Changing my nvim configuraiton.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,46 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colors"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comments"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "nvim-tree".setup {}
-require "user.bufferlines"
-require "user.toggleterm"
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end
+})
+
+local job_id = 0
+vim.keymap.set("n", "<space>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+
+  job_id = vim.bo.channel
+end)
+
+local current_command = ""
+
+vim.keymap.set("n", "<space>te", function()
+  current_command = vim.fn.input("Command: ")
+end)
+
+vim.keymap.set("n", "<space>tr", function()
+  if current_command == "" then
+    current_command = vim.fn.input("Command: ")
+  end
+
+  vim.fn.chansend(job_id, { current_command .. "\r\n" })
+end)
