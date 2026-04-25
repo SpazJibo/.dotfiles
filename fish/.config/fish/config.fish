f5b373b6dba7380b9c1cab17885bf82e19fbf5d9
Curtis Allen Gibeaut Jr. <curtjibo@gmail.com>
2026-04-25 09:34:55 -0500

    Adding default fish config.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,7 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+# function fish_greeting
+# end
+set -gx LD_LIBRARY_PATH /usr/lib \$LD_LIBRARY_PATH
