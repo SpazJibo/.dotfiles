0b8229ad638d699639bba956c41d42db326669dd
Curtis Allen Gibeaut Jr. <curtjibo@gmail.com>
2026-04-25 09:36:58 -0500

    Updating fish configuration for SSH Agent.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,9 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # function fish_greeting
 # end
 set -gx LD_LIBRARY_PATH /usr/lib \$LD_LIBRARY_PATH
+
+if not set -q SSH_AUTH_SOCK
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
